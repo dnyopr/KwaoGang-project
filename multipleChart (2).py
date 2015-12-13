eb56287ca52d0dc9c8dda41f2ffa2d4d86fc0b4c
kawinsmathi <thege3236@gmail.com>
2015-12-13 23:19:54 +0700

    แก้ไข และเพิ่มไฟล์ Embed plot

--- multipleChart (2).py
+++ multipleChart (2).py
@@ -1,43 +1,48 @@
-'''
-Show graphs by using data from lists.
-
-    Lines below are kind of lists.
-1. All population
-    over fifteen = lst[0]
-    total labor force = lst[1]
-    employed = lst[2]
-    unemployed = lst[3]
-    seasonally inactive labor force = lst[4]
-    person not in labor force = lst[5]
-2. Male population
-    over fifteen = lst[7]
-    total labor force = lst[8]
-    employed = lst[9]
-    unemployed = lst[10]
-    seasonally inactive labor force = lst[11]
-    person not in labor force = lst[12]
-3. Female population
-    over fifteen = lst[14]
-    total labor force = lst[15]
-    employed = lst[16]
-    unemployed = lst[17]
-    seasonally inactive labor force = lst[18]
-    person not in labor force = lst[19]
-'''
-
 import csv
 import plotly.plotly as py
 from plotly.graph_objs import *
 with open('test1.csv',newline='') as csvfile:
     a = csv.reader(csvfile)
-    lst = [(','.join(row)).split('\t') for row in a]
+    lst = []
+    for row in a:
+        lst.append((','.join(row)).split('\t'))
+def main(lst):
+    all_population(lst)
+    male_population(lst)
+    female_population(lst)
+
+def all_population(lst):
+    over_fifteen = list(map(int, lst[0]))
+    total_labor_force = list(map(int, lst[1]))
+    employed = list(map(int, lst[2]))
+    unemployed = list(map(int, lst[3]))
+    seasonally_inactive_labor_force = list(map(int, lst[4]))
+    person_not_in_labor_force = list(map(int, lst[5]))
+
+def male_population(lst):
+    over_fifteen_m = list(map(int, lst[7]))
+    total_labor_force_m = list(map(int, lst[8]))
+    employed_m = list(map(int, lst[9]))
+    unemployed_m = list(map(int, lst[10]))
+    seasonally_inactive_labor_force_m = list(map(int, lst[11]))
+    person_not_in_labor_force_m = list(map(int, lst[12]))
+
+
+def female_population(lst):
+    over_fifteen_f = list(map(int, lst[14]))
+    total_labor_force_f = list(map(int, lst[15]))
+    employed_f = list(map(int, lst[16]))
+    unemployed_f = list(map(int, lst[17]))
+    seasonally_inactive_labor_force_f = list(map(int, lst[18]))
+    person_not_in_labor_force_f = list(map(int, lst[19]))
+
 
 def create_all_chart(lst):
     count = 0
     for year in range(10):
         py.sign_in('it58070041', 'bxhjqg6nps')
         trace1 = Bar(
-            x=['All_population', 'Male_population', 'Female_population'],
+            x=['All population', 'Male population', 'Female population'],
             y=[lst[0][count], lst[7][count], lst[14][count]],
             marker=Marker(
                 color='#86a2d7'
@@ -45,7 +50,7 @@ def create_all_chart(lst):
             name='Over fifteen'
         )
         trace2 = Bar(
-            x=['All_population', 'Male_population', 'Female_population'],
+            x=['All population', 'Male population', 'Female population'],
             y=[lst[1][count], lst[8][count], lst[15][count]],
             marker=Marker(
                 color='#3aa0ad'
@@ -53,7 +58,7 @@ def create_all_chart(lst):
             name='Total labor force'
         )
         trace3 = Bar(
-            x=['All_population', 'Male_population', 'Female_population'],
+            x=['All population', 'Male population', 'Female population'],
             y=[lst[2][count], lst[9][count], lst[16][count]],
             marker=Marker(
                 color='#6b80ce'
@@ -61,7 +66,7 @@ def create_all_chart(lst):
             name='Employed'
         )
         trace4 = Bar(
-            x=['All_population', 'Male_population', 'Female_population'],
+            x=['All population', 'Male population', 'Female population'],
             y=[lst[3][count], lst[10][count], lst[17][count]],
             marker=Marker(
                 color='#3b86b2'
@@ -69,7 +74,7 @@ def create_all_chart(lst):
             name='Unemployed'
         )
         trace5 = Bar(
-            x=['All_population', 'Male_population', 'Female_population'],
+            x=['All population', 'Male population', 'Female population'],
             y=[lst[4][count], lst[11][count], lst[18][count]],
             marker=Marker(
                 color='#2d4686'
@@ -77,7 +82,7 @@ def create_all_chart(lst):
             name='Seasonally inactive labor force'
         )
         trace6 = Bar(
-            x=['All_population', 'Male_population', 'Female_population'],
+            x=['All population', 'Male population', 'Female population'],
             y=[lst[5][count], lst[12][count], lst[19][count]],
             marker=Marker(
                 color='#1b3c50'
